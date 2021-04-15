d8f5b8f1e98b155ba4a4134e243851736c79c6b9
Ivan Gagis <igagis@gmail.com>
2021-04-15 14:32:52 +0300

    some doxygen

--- src/tst/application.hpp
+++ src/tst/application.hpp
@@ -13,6 +13,14 @@
 
 namespace tst{
 
+/**
+ * @brief Test runner application.
+ * The application class provides an entry point for the test program.
+ * In case one needs more flexibility on how the test program is being started/finalized
+ * then one can subclass the application class and override some of it's behaviour.
+ * In that case, one also has to provide the tst::create_application() factory function
+ * which will construct the application instance.
+ */
 class application{
 	friend int main(utki::span<const char*> args);
 	friend class reporter;
@@ -38,6 +46,13 @@ class application{
 	void read_run_list_from_stdin();
 
 public:
+	/**
+	 * @brief Constructor.
+	 * @param name - the test application name.
+	 *               This name will be shown in the test application's help output.
+	 * @param description - description of the test application.
+	 *                      This description will be shown in the test application's help output.
+	 */
 	application(
 			std::string&& name,
 			std::string&& description
@@ -45,13 +60,40 @@ public:
 
 	virtual ~application(){}
 
+	/**
+	 * @brief Initialize test cases.
+	 * This function is called by tst right after command line arguments have been parsed.
+	 * The default implementation of the function adds test cases to the application
+	 * from all tst::set instances.
+	 */
 	virtual void init();
 
+	/**
+	 * @brief Command line arguments parser.
+	 * This is the instance of the command line arguments parser.
+	 * Command line arguments parsing is invoked by tst right after the application instance is created.
+	 * So, in case one needs to add some custom command line arguments to the parser,
+	 * then one can do it from within the constructor of the application subclass.
+	 */
 	clargs::parser cli;
 
+	/**
+	 * @brief Get test suite.
+	 * This function tries to search for the test suite with the given id.
+	 * In case the suite is not found, it will be created.
+	 * @param id - the id of the test suite to get.
+	 * @return reference to the test suite with the requested id.
+	 */
 	suite& get_suite(const std::string& id);
 };
 
+/**
+ * @brief Application factory function.
+ * In case user suclasses the application class, he/she needs to provide an implementation of the
+ * factory function for creating an instance of the subclassed application.
+ * In case an implementation of the factory function is not provided, the tst will create an instance
+ * of tst::application class with deafult behaviour.
+ */
 #if M_OS == M_OS_WINDOWS
 __declspec(dllexport)
 #endif
--- src/tst/check.hpp
+++ src/tst/check.hpp
@@ -9,23 +9,56 @@
 
 namespace tst{
 
+/**
+ * @brief Check for condition with additional failure information.
+ * The function checks for the condition to be true.
+ * In case the condition is true, the function does nothing.
+ * In case the condition is false, the function prepares a failure message by
+ * calling the provided function and throws an exception containing the failure information.
+ * @param c - condition to check.
+ * @param print - function performing output of addional failure message information.
+ * @param source_location - object with source file:line information.
+ */
 void check(
 		bool c,
 		const std::function<void(std::ostream&)>& print,
 		const std::pair<const char*, size_t>& source_location
 	);
 
-#define CHECK2(condition, print) tst::check((condition), (print), SL)
+#define CHECK_INTERNAL2(condition, print) tst::check((condition), (print), SL)
 
+/**
+ * @brief Check for condition.
+ * The function checks for the condition to be true.
+ * In case the condition is true, the function does nothing.
+ * In case the condition is false, the function throws an exception.
+ * @param c - condition to check.
+ * @param source_location - object with source file:line information.
+ */
 void check(
 		bool c,
 		const std::pair<const char*, size_t>& source_location
 	);
 
-#define CHECK1(condition) tst::check((condition), SL)
+#define CHECK_INTERNAL1(condition) tst::check((condition), SL)
 
-#define CHECK(...) UTKI_GET_MACRO(__VA_ARGS__, _10, _9, _8, _7, _6, _5, _4, _3, CHECK2, CHECK1)(__VA_ARGS__)
+/**
+ * @brief Check for condition macro.
+ * This is a convenience macro which wraps a call to tst::check() function.
+ * It hides the need of typing the trailing source_location argument of the function.
+ */
+#define CHECK(...) UTKI_GET_MACRO(__VA_ARGS__, _10, _9, _8, _7, _6, _5, _4, _3, CHECK_INTERNAL2, CHECK_INTERNAL1)(__VA_ARGS__)
 
+/**
+ * @brief Check for equality.
+ * This is a convenience function which checks for equality of two values.
+ * Under the hood it calls to tst::check(), but also, it automatically print the
+ * input values in case of check failure.
+ * @param a - fisrt value.
+ * @param b - second value.
+ * @param print - function performing output of addional failure message information.
+ * @param source_location - object with source file:line information.
+ */
 template <class parameter>
 void check_eq(
 		const parameter& a,
@@ -47,8 +80,17 @@ void check_eq(
 		);
 }
 
-#define CHECK_EQ3(a, b, print) tst::check_eq((a), (b), (print), SL)
+#define CHECK_EQ_INTERNAL3(a, b, print) tst::check_eq((a), (b), (print), SL)
 
+/**
+ * @brief Check for equality.
+ * This is a convenience function which checks for equality of two values.
+ * Under the hood it calls to tst::check(), but also, it automatically print the
+ * input values in case of check failure.
+ * @param a - fisrt value.
+ * @param b - second value.
+ * @param source_location - object with source file:line information.
+ */
 template <class parameter>
 void check_eq(
 		const parameter& a,
@@ -59,8 +101,13 @@ void check_eq(
 	check_eq(a, b, nullptr, source_location);
 }
 
-#define CHECK_EQ2(a, b) tst::check_eq((a), (b), SL)
+#define CHECK_EQ_INTERNAL2(a, b) tst::check_eq((a), (b), SL)
 
-#define CHECK_EQ(...) UTKI_GET_MACRO(__VA_ARGS__, _10, _9, _8, _7, _6, _5, _4, CHECK_EQ3, CHECK_EQ2)(__VA_ARGS__)
+/**
+ * @brief Check for equality macro.
+ * This is a convenience macro which wraps a call to tst::check_eq() function.
+ * It hides the need of typing the trailing source_location argument of the function.
+ */
+#define CHECK_EQ(...) UTKI_GET_MACRO(__VA_ARGS__, _10, _9, _8, _7, _6, _5, _4, CHECK_EQ_INTERNAL3, CHECK_EQ_INTERNAL2)(__VA_ARGS__)
 
 }
