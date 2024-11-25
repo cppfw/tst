a2270c8697fc8c1d0e2f31585cd8e4f16dfd10ab
Ivan Gagis <igagis@gmail.com>
2024-11-25 22:51:56 +0200

    vcpkg: fix test

--- vcpkg/test/main.cpp
+++ vcpkg/test/main.cpp
@@ -2,7 +2,7 @@
 #include <tst/check.hpp>
 
 namespace{
-const tst::set("test_set", [](tst::suite& suite){
+const tst::set set("test_set", [](tst::suite& suite){
     suite.add("sample_test", [](){
         std::string s = "Hi!";
         tst::check_eq(s.size(), size_t(3), SL);
